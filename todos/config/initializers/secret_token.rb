57cd0e7966fd698013d9acd0301ede0e3c229218
Piotr Sarnacki <drogus@gmail.com>
2011-04-06 14:58:25 +0200

    Added rails app

---
+++ todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'a22d47b527addcff1cbae597efbeb78e5b195664a9c65fdec5bf4deea6666b215b4cfc70de20f69a7ebe457078b5a07438eb23d6de46a28154e9ff907d5bd8ce'
